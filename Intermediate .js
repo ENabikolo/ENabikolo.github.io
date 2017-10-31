d74dc6b8197c663276c2186f673e4326e7cf5db5
Esther Nabikolo <enabikolo@qa.com>
2017-10-31 15:34:45 +0000

    Working files

--- Intermediate .js
+++ Intermediate .js
@@ -1,8 +1,26 @@
+
 //Iteration 3
 for(i = 0; i < 100; i++){
     if(i/3){
         console.log("Fizz"); 
-    }else if{
-        ()
+    }else if(i/5){
+        console.log("Buzz"); 
+    } else {
+        (i/3 && i/5);
+        console.log("Fizz Buzz"); 
     }
-}
+    
+    }
+       
+    //Iteration 4
+    for(i=0; i<25; i++){
+        Console.log()
+    }
+ 
+
+
+    //Strings 4
+    let j;
+    for (j = 0; j < 25; j++){
+        console.log(j)
+    } 
